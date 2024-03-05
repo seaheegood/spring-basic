d58bb88e53f01187b0d88f201f1beed7d739bff4
Sehee Hong <seaheehong@gmail.com>
2024-03-05 19:33:55 +0900

    Member domain init

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 동시성 이슈가 발생할 수 있지만, 개발 용도로만 단순하게 HashMap 사용
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}

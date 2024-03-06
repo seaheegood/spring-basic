f71406c5746e671621ebfcdb8f4812c2952ed166
Sehee Hong <seaheehong@gmail.com>
2024-03-07 05:41:09 +0900

    separation of interests : AppConfig 를 통한 의존관계 주입

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,15 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     // rely on abstraction(추상화) and embodiment(구체화)
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // 현재 이 멤버 서비스 인플에 MemoryMemberRepository 에 관한 코드가 전혀 없다
+    // 오로지 MemberRepository 라는 인터페이스만이 존재한다
+    // -> 추상화에만 의존하는 것 DIP 를 지키는 것
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,33 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    /**
+     * 위의 주석과 같이 설계하면 다음을 충족
+     *
+     * 역할과 구현을 충실하게 분리
+     * 다형성 활용 -> 인터페이스와 구현 객체를 분리
+     * OrderServiceImpl 은 DiscountPolicy 와 FixDiscountPolicy & RateDiscountPolicy
+     * 추상, 구현 클래스 모두에 의존한다 -> DIP 원칙 위반
+     * 클라이언트 쪽 코드를 건들지 않고서는 기능을 확장할 수 없다 -> OCP 위반
+     */
+    //해결 -> 누군가가 클라이언트인 OrderServiceImpl 에 DiscountPolicy 의 구현 객체를 대신 생성하고 주입해주어야 함
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
